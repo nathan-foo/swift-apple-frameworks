0149df8107946e08894e5ad3adeacda749fe228e
Nathan Foo <ntfoo27@outlook.com>
2025-02-21 15:05:49 -0600

    Add safari view

--- AppleFrameworks/FrameworkDetailView.swift
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -11,6 +11,7 @@ struct FrameworkDetailView: View {
     
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
@@ -38,11 +39,16 @@ struct FrameworkDetailView: View {
             Spacer()
             
             Button {
-                
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
         }
+        // or sheet
+        .fullScreenCover(isPresented: $isShowingSafariView,
+               content: {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+        })
     }
 }
 
